f913d4058738219afa8a3baa699a8d5212fc4f23
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-02-22 15:40:13 +0600

    before gql

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -1,9 +1,64 @@
+import { Center, Stack, Text, Button, Image, Input } from "@chakra-ui/react";
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 interface IAuthProps {
+  session: Session | null;
+  reloadSession: () => void;
 }
 
-const Auth: React.FunctionComponent<IAuthProps> = (props) => {
-  return <div></div>;
+const Auth: React.FunctionComponent<IAuthProps> = ({
+  session,
+  reloadSession,
+}) => {
+  const [username, setUsername] = useState("");
+
+  const onSubmit = async () => {
+    try {
+      // create username mutation by gql
+    } catch (error) {
+      console.log("onsubmit error", error);
+    }
+  };
+
+  return (
+    <Center height="100vh">
+      <Stack align="center" spacing="8">
+        {session ? (
+          <>
+            <Text fontSize="3xl">Create a username</Text>
+            <Input
+              placeholder="Enter a username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <Button width="100%" onClick={onSubmit}>
+              Submit
+            </Button>
+          </>
+        ) : (
+          <>
+            <Text fontSize="3xl">MessengerQL</Text>
+            <Button
+              leftIcon={
+                <Image
+                  alt="google logo"
+                  height="20px"
+                  src="/images/googlelogo.png"
+                />
+              }
+              onClick={() => signIn("google")}
+            >
+              Continue with Google
+            </Button>
+          </>
+        )}
+      </Stack>
+    </Center>
+  );
 };
 
 export default Auth;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,29 @@
 import Auth from "@/components/Auth/Auth";
 import Chat from "@/components/Chat/Chat";
-import { Box } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/react";
 import { Inter } from "@next/font/google";
 import { NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
-import { Session } from 'next-auth'
+import { Session } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { data } = useSession();
+  const { data: session } = useSession();
 
-  console.log("Here is data", data);
+  console.log("Here is session", session);
+
+  const reloadSession = () => {};
 
   return (
-    <>    
-      <Box>{ data?.user ? <Chat/> : <Auth/> }</Box>
+    <>
+      <Box>
+        {session?.user.username ? (
+          <Chat />
+        ) : (
+          <Auth session={session} reloadSession={reloadSession} />
+        )}
+      </Box>
     </>
   );
 }
