441e204af5ba3e6e96a5d5fa74486aeee046f5ef
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-02-22 14:54:58 +0600

    hmm

---
+++ frontend/src/components/Auth/Auth.tsx
@@ -0,0 +1,9 @@
+
+interface IAuthProps {
+}
+
+const Auth: React.FunctionComponent<IAuthProps> = (props) => {
+  return <div></div>;
+};
+
+export default Auth;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,10 @@
+import Auth from "@/components/Auth/Auth";
+import Chat from "@/components/Chat/Chat";
+import { Box } from '@chakra-ui/react';
 import { Inter } from "@next/font/google";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { NextPageContext } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { Session } from 'next-auth'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,15 +14,18 @@ export default function Home() {
   console.log("Here is data", data);
 
   return (
-    <>
-      <div>
-        {data?.user ? (
-          <button onClick={() => signOut()}>Sign Out</button>
-        ) : (
-          <button onClick={() => signIn("google")}>Sign In</button>
-        )}
-        { data?.user?.name }
-      </div>
+    <>    
+      <Box>{ data?.user ? <Chat/> : <Auth/> }</Box>
     </>
   );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
